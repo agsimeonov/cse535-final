7437893a182262b00529481abbe144da2bde49f8
Alexander Simeonov <agsimeon@buffalo.edu>
2015-11-28 03:24:33 -0500

    Some more musing into pie charts with highcharts.

--- crossdoc/pie.py
+++ crossdoc/pie.py
@@ -3,6 +3,7 @@ from highcharts import Highchart
 
 chart = Highchart()
 
-data = range(1,20)
+data = {"United States" : 2 , "Greece" : 4, "France" : 3}
+data = data.items()
 chart.add_data_set(data, series_type='pie', name='Results per Country')
 chart.save_file("pie")

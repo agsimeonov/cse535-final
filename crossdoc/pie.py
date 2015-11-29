db451dd38630a5b837bad370f007e12786600dfe
Alexander Simeonov <agsimeon@buffalo.edu>
2015-11-28 20:19:48 -0500

    Mostly implemented a pie chart per lang.

--- crossdoc/pie.py
+++ crossdoc/pie.py
@@ -1,9 +1,25 @@
+import json
+
 from highcharts import Highchart
 
 
-chart = Highchart()
+jsonInput = '{"response":{"numFound":3,"start":0,"docs":[{"lang":"en"},{"lang":"ru"},{"lang":"de"},{"lang":"de"},{"lang":"de"},{"lang":"ar"},{"lang":"ru"},{"lang":"fr"},{"lang":"ru"}]}}'
 
-data = {"United States" : 2 , "Greece" : 4, "France" : 3}
-data = data.items()
-chart.add_data_set(data, series_type='pie', name='Results per Country')
+jsonInput = json.loads(jsonInput)
+docs = jsonInput['response']['docs']
+langs = {}
+
+for doc in docs:
+  lang = doc['lang']
+  lang = lang.upper()
+  if lang in langs:
+    langs[lang] += 1
+  else:
+    langs[lang] = 1
+
+chart = Highchart()
+options = {'title' : {'text' : 'Results per Language'}} 
+chart.set_dict_options(options)
+data = langs.items()
+chart.add_data_set(data, series_type='pie', name='Results')
 chart.save_file("pie")

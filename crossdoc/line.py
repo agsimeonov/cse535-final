58a22cc01587fc82876d7c21c5d018c5cd30fe3a
Alexander Simeonov <agsimeon@buffalo.edu>
2015-11-30 15:13:00 -0500

    Progress towards line chart.

--- crossdoc/line.py
+++ crossdoc/line.py
@@ -1,5 +1,16 @@
+from json import loads
+from os.path import splitext
 from sys import argv, exit
 
+from highcharts.highcharts.highcharts import Highchart
+
+
+DOCS       = 'docs'
+RESPONSE   = 'response'
+CREATED_AT = 'created_at'
+HASHTAGS   = 'tweet_hashtags'
+TITLE    = 'title'
+TEXT     = 'text'
 
 # Make sure we have the correct command line arguments
 if len(argv) != 3:
@@ -7,4 +18,47 @@ if len(argv) != 3:
   print "python pie.py <JSON Query Results> <Line Chart Output File>"
   exit(0)
 
-# TODO  
+jsonInput = loads(argv[1])
+jsonInput = loads('{"response":{"numFound":3,"docs":[{"created_at":"2015-11-28T14:32:44Z","tweet_hashtags":["a","b","c"]},{"created_at":"2015-11-28T14:32:44Z","tweet_hashtags":["a","b","c"]},{"created_at":"2015-11-28T14:32:44Z","tweet_hashtags":["a","b","c"]},{"created_at":"2015-11-29T14:32:44Z","tweet_hashtags":["b","c","d"]},{"created_at":"2015-11-30T14:32:44Z","tweet_hashtags":["c","d","e"]},{"created_at":"2015-11-30T14:32:44Z","tweet_hashtags":["c","d","e"]}]}}')
+if RESPONSE in jsonInput:
+  if DOCS in jsonInput[RESPONSE]:
+    docs = jsonInput[RESPONSE][DOCS]
+  else:
+    print "'docs' list not found in JSON 'response'!"
+    exit(0)
+else:
+  print "'response' dictionary not found in JSON!"
+  exit(0)    
+
+data = {}
+
+for doc in docs:
+  if not CREATED_AT in doc:
+    continue
+  if not HASHTAGS in doc:
+    continue
+  
+  createdAt = doc[CREATED_AT][:10]
+  hashtags = doc[HASHTAGS]
+  
+  if not hashtags:
+    continue
+  
+  if createdAt in data:
+    counts = data[createdAt]
+  else:
+    counts = {}
+    
+  for hashtag in hashtags:
+    if hashtag in counts:
+      counts[hashtag] += 1
+    else:
+      counts[hashtag] = 1
+  
+  data[createdAt] = counts
+  
+chart = Highchart()
+options = {TITLE : {TEXT : 'Results per Language'}} 
+chart.set_dict_options(options)
+chart.add_data_set(data.items(), series_type='line', name='Results')
+chart.save_file(splitext(argv[2])[0]) 
--- crossdoc/pie.py
+++ crossdoc/pie.py
@@ -5,6 +5,13 @@ from sys import argv, exit
 from highcharts import Highchart
 
 
+DOCS     = 'docs'
+RESPONSE = 'response'
+LANG     = 'lang'
+COUNTRY  = 'country'
+TITLE    = 'title'
+TEXT     = 'text'
+
 # Make sure we have the correct command line arguments
 if len(argv) != 4:
   print "Please provide command line arguments as follows:"
@@ -12,29 +19,30 @@ if len(argv) != 4:
   exit(0)
 
 jsonInput = loads(argv[1])
-if 'response' in jsonInput:
-  if 'docs' in jsonInput['response']:
-    docs = jsonInput['response']['docs']
+if RESPONSE in jsonInput:
+  if DOCS in jsonInput[RESPONSE]:
+    docs = jsonInput[RESPONSE][DOCS]
   else:
     print "'docs' list not found in JSON 'response'!"
     exit(0)
 else:
   print "'response' dictionary not found in JSON!"
   exit(0)
+
 langs = {}
 countries = {}
 
 for doc in docs:
-  if 'lang' in doc:
-    lang = doc['lang']
+  if LANG in doc:
+    lang = doc[LANG]
     if lang:
       lang = lang.upper()
       if lang in langs:
         langs[lang] += 1
       else:
         langs[lang] = 1
-  if 'country' in doc:
-    country = doc['country']
+  if COUNTRY in doc:
+    country = doc[COUNTRY]
     if country:
       if country in countries:
         countries[country] += 1
@@ -42,13 +50,13 @@ for doc in docs:
         countries[country] = 1
 
 chart = Highchart()
-options = {'title' : {'text' : 'Results per Language'}} 
+options = {TITLE : {TEXT : 'Results per Language'}} 
 chart.set_dict_options(options)
 chart.add_data_set(langs.items(), series_type='pie', name='Results')
 chart.save_file(splitext(argv[2])[0])
 
 chart = Highchart()
-options = {'title' : {'text' : 'Results per Country'}} 
+options = {TITLE : {TEXT : 'Results per Country'}} 
 chart.set_dict_options(options)
 chart.add_data_set(countries.items(), series_type='pie', name='Results')
 chart.save_file(splitext(argv[3])[0])
